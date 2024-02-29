8664d207757187849a034e2eab9912864e2d0c7e
kigongo <igakigongo@gmail.com>
2024-02-29 21:58:57 +0100

    Annalyn's Infilitration

---
+++ AnnalynsInfiltration/QuestLogic.cs
@@ -0,0 +1,16 @@
+﻿namespace AnnalynsInfiltration;
+
+public static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+        => knightIsAwake || archerIsAwake || prisonerIsAwake;
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+        => !archerIsAwake && prisonerIsAwake;
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake,
+        bool prisonerIsAwake, bool petDogIsPresent)
+        => !archerIsAwake && (petDogIsPresent || (prisonerIsAwake && !knightIsAwake));
+}
