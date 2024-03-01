8ab9ddb84408f7c75563a239d8e68b41e28aace3
kigongo <igakigongo@gmail.com>
2024-03-01 22:10:42 +0100

    car assemble

---
+++ CarsAssemble/CarsAssemble.cs
@@ -0,0 +1,19 @@
+﻿namespace CarsAssemble;
+
+public static class AssemblyLine
+{
+    public static double SuccessRate(int speed) => speed switch
+    {
+        0 => 0.00,
+        <= 4 => 1.00,
+        <= 8 => 0.90,
+        <= 9 => 0.80,
+        _ => 0.77
+    };
+
+    public static double ProductionRatePerHour(int speed)
+        => speed * 221 * SuccessRate(speed);
+
+    public static int WorkingItemsPerMinute(int speed)
+        => (int)(ProductionRatePerHour(speed) / 60);
+}
