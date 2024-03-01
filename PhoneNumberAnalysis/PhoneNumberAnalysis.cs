267133959bca566dd3baffc703f68a6655be19be
kigongo <igakigongo@gmail.com>
2024-03-01 21:47:48 +0100

    phone number analysis

---
+++ PhoneNumberAnalysis/PhoneNumberAnalysis.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoneNumberAnalysis;
+
+public static class PhoneNumber
+{
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+    {
+        var tokens = phoneNumber.Split('-');
+        return (tokens[0] == "212", tokens[1] == "555", tokens[2]);
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+        => phoneNumberInfo.IsFake;
+}
