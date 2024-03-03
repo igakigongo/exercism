1c0844bf18b21f04a12d34eec9f44ad83315b669
kigongo <igakigongo@gmail.com>
2024-03-04 00:34:22 +0100

    elons toys

---
+++ ElonsToys/ElonsToys.cs
@@ -0,0 +1,25 @@
+﻿namespace ElonsToys;
+
+public class RemoteControlCar
+{
+    private int _batteryPercentage = 100;
+    private int _distanceInMeters;
+
+
+    public static RemoteControlCar Buy()
+        => new();
+
+    public string DistanceDisplay()
+        => $"Driven {_distanceInMeters} meters";
+
+    public string BatteryDisplay()
+        => _batteryPercentage == 0 ? "Battery empty" : $"Battery at {_batteryPercentage}%";
+
+    public void Drive()
+    {
+        if (_batteryPercentage == 0) return;
+
+        _distanceInMeters += 20;
+        _batteryPercentage -= 1;
+    }
+}
