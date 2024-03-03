62bd9ef7b6ed48bb6de3785cbeefa9d4ecad082f
kigongo <igakigongo@gmail.com>
2024-03-04 00:07:03 +0100

    roll the die

---
+++ RollTheDie/RollTheDie.cs
@@ -0,0 +1,10 @@
+﻿namespace RollTheDie;
+
+public class Player
+{
+    private readonly Random _random = new((int)DateTime.UtcNow.Ticks);
+
+    public int RollDie() => _random.Next(1, 18);
+
+    public double GenerateSpellStrength() => _random.NextDouble() * 100;
+}
