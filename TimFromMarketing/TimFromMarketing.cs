746a11f58fc6a46866edd45795ed0aa13f2ef73b
kigongo <igakigongo@gmail.com>
2024-03-03 23:33:19 +0100

    tim from marketing

---
+++ TimFromMarketing/TimFromMarketing.cs
@@ -0,0 +1,11 @@
+﻿namespace TimFromMarketing;
+
+public static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {
+        var result = id == null ? string.Empty: $"[{id}] - ";
+        result += $"{name} - {(department ?? "owner").ToUpper()}";
+        return result;
+    }
+}
