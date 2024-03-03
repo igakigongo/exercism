bfadd0e9822b453a03bfc5deab3f4927efe2db56
kigongo <igakigongo@gmail.com>
2024-03-03 23:54:51 +0100

    darts

---
+++ Darts/Darts.cs
@@ -0,0 +1,16 @@
+﻿namespace Darts;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        var distanceFromCentre = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        return distanceFromCentre switch
+        {
+            <= 1 => 10,
+            > 1 and <= 5 => 5,
+            > 5 and <= 10 => 1,
+            _ => 0
+        };
+    }
+}
