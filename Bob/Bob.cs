eebde95423f99de85521aeb809d0540e5b65b3e1
kigongo <igakigongo@gmail.com>
2024-03-04 21:48:25 +0100

    update bob

--- Bob/Bob.cs
+++ Bob/Bob.cs
@@ -1,48 +1,32 @@
-using System.Text.RegularExpressions;
-
 namespace Bob;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        statement = statement.Trim();
-        if (string.IsNullOrWhiteSpace(statement) ||
-            string.IsNullOrEmpty(statement) ||
-            IsWhiteSpace(statement))
-            return "Fine. Be that way!";
-
-        uint result = 0b_0;
-        if (IsQuestion(statement)) result |= 0b_1; // 1
-        if (IsYelling(statement)) result |= 0b_10; // 2
-
-        return result switch
+        switch (statement.Trim())
         {
-            0b_1 => "Sure.",
-            0b_10 => "Whoa, chill out!",
-            0b_11 => "Calm down, I know what I'm doing!", // 3
-            _ => "Whatever."
-        };
+            case { } message when string.IsNullOrWhiteSpace(message):
+                return "Fine. Be that way!";
+
+            case { } message when IsYelling(message) && IsQuestion(message):
+                return "Calm down, I know what I'm doing!";
+
+            case { } message when IsYelling(message):
+                return "Whoa, chill out!";
+
+            case { } message when IsQuestion(message):
+                return "Sure.";
+
+            default:
+                return "Whatever.";
+        }
     }
 
     private static bool IsQuestion(string statement) =>
         statement[^1] == '?';
 
     private static bool IsYelling(string statement)
-    {
-        var letters = statement.Where(IsLetter).ToList();
-        return letters.Count != 0 && letters.All(IsUpperCaseLetter);
-    }
-
-    private static bool IsLetter(char c) =>
-        IsLowerCaseLetter(c) || IsUpperCaseLetter(c);
-
-    private static bool IsLowerCaseLetter(char c) =>
-        c >= 97 && c <= 122;
-
-    private static bool IsUpperCaseLetter(char c) =>
-        c >= 65 && c <= 90;
-
-    private static bool IsWhiteSpace(string statement) =>
-        new Regex(@"^[\s\r\n\t]+$", RegexOptions.Compiled).IsMatch(statement);
+        => statement.Any(char.IsLetter) &&
+           statement.Equals(statement.ToUpperInvariant(), StringComparison.Ordinal);
 }
