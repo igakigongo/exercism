637b8a108ca90ea1f0e70f811cf2acada4eec181
kigongo <igakigongo@gmail.com>
2024-03-05 23:44:16 +0100

    reverse string

---
+++ ReverseString/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿namespace ReverseString;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var arr = input.ToCharArray();
+        for (int i = 0, j = input.Length - 1; i <= j; i++, j--)
+        {
+            (arr[i], arr[j]) = (arr[j], arr[i]);
+        }
+
+        return new string(arr);
+    }
+}
