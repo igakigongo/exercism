dbaed97628eb7a5e52145abba15a46b6a9e56098
kigongo <igakigongo@gmail.com>
2024-03-05 21:42:46 +0100

    space age

---
+++ SpaceAge/SpaceAge.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceAge;
+
+public class SpaceAge(int seconds)
+{
+    public double OnEarth() => seconds / (365.25 * 24 * 60 * 60);
+
+    public double OnMercury() => OnEarth() / 0.2408467;
+
+    public double OnVenus() => OnEarth() / 0.61519726;
+
+    public double OnMars() => OnEarth() / 1.8808158;
+
+    public double OnJupiter() => OnEarth() / 11.862615;
+
+    public double OnSaturn() => OnEarth() / 29.447498;
+
+    public double OnUranus() => OnEarth() / 84.016846;
+
+    public double OnNeptune() => OnEarth() / 164.79132;
+}
