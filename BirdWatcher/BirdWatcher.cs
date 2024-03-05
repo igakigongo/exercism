9aa373a72f0d7abf240369167e21bf7de8703822
kigongo <igakigongo@gmail.com>
2024-03-05 23:11:22 +0100

    bird watcher

---
+++ BirdWatcher/BirdWatcher.cs
@@ -0,0 +1,23 @@
+﻿namespace BirdWatcher;
+
+public class BirdCount(int[] birdsPerDay)
+{
+    private static readonly int[] LastWeekStats = [0, 2, 5, 3, 7, 8, 4];
+
+    public static int[] LastWeek() => LastWeekStats;
+
+    public int Today() => birdsPerDay[^1];
+
+    public void IncrementTodaysCount() => birdsPerDay[^1]++;
+
+    public bool HasDayWithoutBirds() => birdsPerDay.Any(x => x == 0);
+
+    public int CountForFirstDays(int numberOfDays)
+    {
+        var sum = 0;
+        for (var i = 0; i < numberOfDays; i++) sum += birdsPerDay[i];
+        return sum;
+    }
+
+    public int BusyDays() => birdsPerDay.Count(x => x >= 5);
+}
