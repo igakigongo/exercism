0fa296986f121d80fc07a06bcb5fd7ab5f35f470
kigongo <igakigongo@gmail.com>
2024-03-07 16:44:07 +0100

    sum of multiples

---
+++ SumOfMultiples/SumOfMultiples.cs
@@ -0,0 +1,19 @@
+﻿namespace SumOfMultiples;
+
+public static class SumOfMultiples
+{
+    public static int Sum(int[] multiples, int max)
+    {
+        if (multiples.Length == 0) return 0;
+
+        var mults = new HashSet<int>();
+        foreach (var factor in multiples)
+        {
+            if (factor == 0) continue;
+            for (var i = factor; i < max; i += factor)
+                mults.Add(i);
+        }
+
+        return mults.Sum();
+    }
+}
