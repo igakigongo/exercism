d46210d668c439b0c7347bbbd2e4ca501fe6f828
kigongo <igakigongo@gmail.com>
2024-03-07 01:55:22 +0100

    binary search

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,24 @@
+﻿namespace BinarySearch;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        int lo = 0, hi = input.Length - 1;
+
+        while (lo <= hi)
+        {
+            var mid = (lo + hi) >>> 1;
+
+            if (input[mid] == value) return mid;
+
+            if (input[mid] < value)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+
+
+        return -1;
+    }
+}
