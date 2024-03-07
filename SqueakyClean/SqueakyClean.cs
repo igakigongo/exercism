0c22da30a43e494c631bfda333e7b67cc00ffad9
kigongo <igakigongo@gmail.com>
2024-03-08 00:36:40 +0100

    squeaky clean

---
+++ SqueakyClean/SqueakyClean.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace SqueakyClean;
+
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < identifier.Length; i++)
+        {
+            var ch = identifier[i];
+            if (char.IsWhiteSpace(ch))
+                sb.Append('_');
+            else if (char.IsControl(ch))
+                sb.Append("CTRL");
+            else if (ch == '-')
+                sb.Append(char.ToUpper(identifier[++i]));
+            else if (char.IsLetter(ch) && ch is < 'α' or > 'ω')
+                sb.Append(ch);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ SqueakyClean/SqueakyCleanTests.cs
@@ -0,0 +1,66 @@
+using Xunit;
+
+namespace SqueakyClean;
+
+public class SqueakyCleanTests
+{
+    [Fact]
+    public void Clean_single_letter()
+    {
+        Assert.Equal("A", Identifier.Clean("A"));
+    }
+
+    [Fact]
+    public void Clean_clean_string()
+    {
+        Assert.Equal("àḃç", Identifier.Clean("àḃç"));
+    }
+
+    [Fact]
+    public void Clean_string_with_spaces()
+    {
+        Assert.Equal("my___Id", Identifier.Clean("my   Id"));
+    }
+
+    [Fact]
+    public void Clean_string_with_control_char()
+    {
+        Assert.Equal("myCTRLId", Identifier.Clean("my\0Id"));
+    }
+
+    [Fact]
+    public void Clean_empty_string()
+    {
+        Assert.Equal(string.Empty, Identifier.Clean(string.Empty));
+    }
+
+    [Fact]
+    public void Convert_kebab_to_camel_case()
+    {
+        Assert.Equal("àḂç", Identifier.Clean("à-ḃç"));
+    }
+
+    [Fact]
+    public void Clean_string_with_special_characters()
+    {
+        Assert.Equal("MyFinder", Identifier.Clean("My😀😀Finder😀"));
+    }
+
+    [Fact]
+    public void Clean_string_with_numbers()
+    {
+        Assert.Equal("MyFinder", Identifier.Clean("1My2Finder3"));
+    }
+
+    [Fact]
+    public void Omit_lower_case_greek_letters()
+    {
+        Assert.Equal("MyΟFinder", Identifier.Clean("MyΟβιεγτFinder"));
+    }
+
+    [Fact]
+    public void Combine_conversions()
+    {
+        Assert.Equal("_AbcĐCTRL", Identifier.Clean("9 -abcĐ😀ω\0"));
+    }
+}
