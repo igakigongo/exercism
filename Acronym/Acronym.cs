27c5d909a5d4e19948481d4e39d9bd3fd87fe0c2
kigongo <igakigongo@gmail.com>
2024-03-08 20:14:24 +0100

    acronym

---
+++ Acronym/Acronym.cs
@@ -0,0 +1,21 @@
+﻿namespace Acronym;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var punctuationMet = true;
+        var result = string.Empty;
+
+        foreach (var ch in phrase)
+            if (char.IsWhiteSpace(ch) || ch == '-')
+                punctuationMet = true;
+            else if (char.IsLetter(ch) && punctuationMet)
+            {
+                result += char.ToUpper(ch);
+                punctuationMet = false;
+            }
+
+        return result;
+    }
+}
