c1f54863965a911a9ddac81ec93c62b13aeb0c37
kigongo <igakigongo@gmail.com>
2024-03-08 23:10:37 +0100

    rotational cipher

---
+++ RotationalCipher/RotationalCipher.cs
@@ -0,0 +1,23 @@
+﻿namespace RotationalCipher;
+
+public static class RotationalCipher
+{
+    public static string Rotate(string text, int shiftKey)
+    {
+        Span<char> span = stackalloc char[text.Length];
+        for (var i = 0; i < text.Length; i++)
+        {
+            var ch = text[i];
+            if (char.IsLetter(ch))
+            {
+                var bas = char.IsUpper(ch) ? 65 : 97;
+                var res = (ch - bas + shiftKey) % 26;
+                span[i] = (char)(res + bas);
+            }
+            else
+                span[i] = ch;
+        }
+
+        return new string(span);
+    }
+}
