dfece3d06cfef1286b0693812a6e321a68d68265
kigongo <igakigongo@gmail.com>
2024-03-09 18:00:30 +0100

    diffie hellman

---
+++ DiffieHellman/DiffieHellman.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace DiffieHellman;
+
+public static class DiffieHellman
+{
+    public static BigInteger PrivateKey(BigInteger primeP)
+        => new Random((int)DateTime.UtcNow.Ticks).NextInt64(2, (long)primeP);
+
+    public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey)
+        => BigInteger.ModPow(primeG, privateKey, primeP);
+
+    public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey)
+        => BigInteger.ModPow(publicKey, privateKey, primeP);
+}
