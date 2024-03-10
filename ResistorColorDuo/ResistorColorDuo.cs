fa995f4b0af5ad67d32fbe5f2f8105459941ca44
kigongo <igakigongo@gmail.com>
2024-03-10 23:12:36 +0100

    resistor color duo

---
+++ ResistorColorDuo/ResistorColorDuo.cs
@@ -0,0 +1,21 @@
+﻿namespace ResistorColorDuo;
+
+public static class ResistorColorDuo
+{
+    private static readonly Dictionary<string, int> Dict = new()
+    {
+        { "black", 0 },
+        { "brown", 1 },
+        { "red", 2 },
+        { "orange", 3 },
+        { "yellow", 4 },
+        { "green", 5 },
+        { "blue", 6 },
+        { "violet", 7 },
+        { "grey", 8 },
+        { "white", 9 }
+    };
+
+    public static int Value(string[] colors) =>
+        Dict[colors[0]] * 10 + Dict[colors[1]];
+}
---
+++ ResistorColorDuo/ResistorColorDuoTests.cs
@@ -0,0 +1,49 @@
+using Xunit;
+
+namespace ResistorColorDuo;
+
+public class ResistorColorDuoTests
+{
+    [Fact]
+    public void Brown_and_black()
+    {
+        Assert.Equal(10, ResistorColorDuo.Value(new[] { "brown", "black" }));
+    }
+
+    [Fact]
+    public void Blue_and_grey()
+    {
+        Assert.Equal(68, ResistorColorDuo.Value(new[] { "blue", "grey" }));
+    }
+
+    [Fact]
+    public void Yellow_and_violet()
+    {
+        Assert.Equal(47, ResistorColorDuo.Value(new[] { "yellow", "violet" }));
+    }
+
+    [Fact]
+    public void White_and_red()
+    {
+        Assert.Equal(92, ResistorColorDuo.Value(new[] { "white", "red" }));
+    }
+
+    [Fact]
+    public void Orange_and_orange()
+    {
+        Assert.Equal(33, ResistorColorDuo.Value(new[] { "orange", "orange" }));
+    }
+
+    [Fact]
+    public void Ignore_additional_colors()
+    {
+        Assert.Equal(51,
+            ResistorColorDuo.Value(new[] { "green", "brown", "orange" }));
+    }
+
+    [Fact]
+    public void Black_and_brown_one_digit()
+    {
+        Assert.Equal(1, ResistorColorDuo.Value(new[] { "black", "brown" }));
+    }
+}
