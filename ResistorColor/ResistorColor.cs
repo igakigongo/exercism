9048afe6d55a37ca4365dca0762c09180c082177
kigongo <igakigongo@gmail.com>
2024-03-10 17:09:44 +0100

    resistor color

---
+++ ResistorColor/ResistorColor.cs
@@ -0,0 +1,13 @@
+﻿namespace ResistorColor;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color) =>
+        Array.IndexOf(Colors(), color);
+
+    public static string[] Colors() =>
+    [
+        "black", "brown", "red", "orange", "yellow", "green", "blue",
+        "violet", "grey", "white"
+    ];
+}
