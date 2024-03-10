f6942c3dca99aad735361a2cf23c5c2f7b24c51c
kigongo <igakigongo@gmail.com>
2024-03-10 21:48:26 +0100

    matching brackets

---
+++ MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,21 @@
+﻿namespace MatchingBrackets;
+
+public static class MatchingBrackets
+{
+    public static bool IsPaired(string input)
+    {
+        var stack = new Stack<char>();
+        foreach (var ch in input)
+            switch (ch)
+            {
+                case '{': stack.Push('}'); break;
+                case '[': stack.Push(']'); break;
+                case '(': stack.Push(')'); break;
+                case '}' or ']' or ')':
+                    if (stack.Count == 0 || stack.Pop() != ch) return false;
+                    break;
+            }
+
+        return stack.Count == 0;
+    }
+}
---
+++ MatchingBrackets/MatchingBracketsTests.cs
@@ -0,0 +1,147 @@
+using Xunit;
+
+namespace MatchingBrackets;
+
+public class MatchingBracketsTests
+{
+    [Fact]
+    public void Paired_square_brackets()
+    {
+        var value = "[]";
+        Assert.True(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Empty_string()
+    {
+        var value = "";
+        Assert.True(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Unpaired_brackets()
+    {
+        var value = "[[";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Wrong_ordered_brackets()
+    {
+        var value = "}{";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Wrong_closing_bracket()
+    {
+        var value = "{]";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Paired_with_whitespace()
+    {
+        var value = "{ }";
+        Assert.True(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Partially_paired_brackets()
+    {
+        var value = "{[])";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Simple_nested_brackets()
+    {
+        var value = "{[]}";
+        Assert.True(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Several_paired_brackets()
+    {
+        var value = "{}[]";
+        Assert.True(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Paired_and_nested_brackets()
+    {
+        var value = "([{}({}[])])";
+        Assert.True(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Unopened_closing_brackets()
+    {
+        var value = "{[)][]}";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Unpaired_and_nested_brackets()
+    {
+        var value = "([{])";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Paired_and_wrong_nested_brackets()
+    {
+        var value = "[({]})";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Paired_and_wrong_nested_brackets_but_innermost_are_correct()
+    {
+        var value = "[({}])";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Paired_and_incomplete_brackets()
+    {
+        var value = "{}[";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Too_many_closing_brackets()
+    {
+        var value = "[]]";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Early_unexpected_brackets()
+    {
+        var value = ")()";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Early_mismatched_brackets()
+    {
+        var value = "{)()";
+        Assert.False(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Math_expression()
+    {
+        var value = "(((185 + 223.85) * 15) - 543)/2";
+        Assert.True(MatchingBrackets.IsPaired(value));
+    }
+
+    [Fact]
+    public void Complex_latex_expression()
+    {
+        var value =
+            "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)";
+        Assert.True(MatchingBrackets.IsPaired(value));
+    }
+}
