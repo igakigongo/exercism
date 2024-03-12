38ecd8bdb823f017285f038d583f164b8001a9a4
kigongo <igakigongo@gmail.com>
2024-03-12 17:07:59 +0100

    proverb

---
+++ Proverb/Proverb.cs
@@ -0,0 +1,15 @@
+﻿namespace Proverb;
+
+public static class Proverb
+{
+    public static IEnumerable<string> Recite(string[] subjects)
+    {
+        if (subjects.Length == 0) yield break;
+        if (subjects.Length > 1)
+            for (var i = 0; i < subjects.Length - 1; i++)
+                yield return
+                    $"For want of a {subjects[i]} the {subjects[i + 1]} was lost.";
+
+        yield return $"And all for the want of a {subjects[0]}.";
+    }
+}
