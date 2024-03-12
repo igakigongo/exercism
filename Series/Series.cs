0e35c534f2ec7268f39775d6a5d78944221603e8
kigongo <igakigongo@gmail.com>
2024-03-13 00:26:55 +0100

    series

---
+++ Series/Series.cs
@@ -0,0 +1,14 @@
+﻿namespace Series;
+
+public static class Series
+{
+    public static IEnumerable<string> Slices(string numbers, int sliceLength)
+    {
+        if (sliceLength <= 0 || sliceLength > numbers.Length)
+            throw new ArgumentException("invalid slice length",
+                nameof(sliceLength));
+
+        for (var i = 0; i <= numbers.Length - sliceLength; i++)
+            yield return numbers[i..(i + sliceLength)];
+    }
+}
