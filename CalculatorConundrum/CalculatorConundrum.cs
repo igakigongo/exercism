ae807e04b48f69d5fd347b97b25fb5765e8a74a4
kigongo <igakigongo@gmail.com>
2024-03-12 19:34:31 +0100

    calculator conundrum

---
+++ CalculatorConundrum/CalculatorConundrum.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculatorConundrum;
+
+using static SimpleOperation;
+
+public static class SimpleCalculator
+{
+    public static string Calculate(int operand1, int operand2, string operation)
+        => operation switch
+        {
+            "+" => FormatResult(operand1, operand2, operation, Addition),
+            "*" => FormatResult(operand1, operand2, operation, Multiplication),
+            "/" => FormatResult(operand1, operand2, operation, Division),
+            "" => throw new ArgumentException("", nameof(operation)),
+            null => throw new ArgumentNullException(nameof(operation)),
+            _ => throw new ArgumentOutOfRangeException(nameof(operation))
+        };
+
+    private static string FormatResult(int operand1, int operand2, string op,
+        Func<int, int, int> func) =>
+        op == "/" && operand2 == 0
+            ? "Division by zero is not allowed."
+            : $"{operand1} {op} {operand2} = {func(operand1, operand2)}";
+}
