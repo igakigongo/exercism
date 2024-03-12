892775682c54113ab8a7f6da9d0f0de16ca9b3a4
kigongo <igakigongo@gmail.com>
2024-03-12 17:24:05 +0100

    lucians luscious lasagna

---
+++ LuciansLusciousLasagna/Lasagna.cs
@@ -0,0 +1,15 @@
+﻿namespace LuciansLusciousLasagna;
+
+public class Lasagna
+{
+    public int ExpectedMinutesInOven() => 40;
+
+    public int RemainingMinutesInOven(int minutesInOven) =>
+        ExpectedMinutesInOven() - minutesInOven;
+
+    public int PreparationTimeInMinutes(int numberOfLayers) =>
+        numberOfLayers * 2;
+
+    public int ElapsedTimeInMinutes(int numberOfLayers, int minutesInOven) =>
+        PreparationTimeInMinutes(numberOfLayers) + minutesInOven;
+}
