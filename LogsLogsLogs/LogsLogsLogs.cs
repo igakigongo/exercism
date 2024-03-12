9817a877d3b7b48ab217aafd97839d338332ba81
kigongo <igakigongo@gmail.com>
2024-03-12 18:39:04 +0100

    logs logs logs

---
+++ LogsLogsLogs/LogsLogsLogs.cs
@@ -0,0 +1,29 @@
+namespace LogsLogsLogs;
+
+public enum LogLevel
+{
+    Unknown = 0,
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42
+}
+
+public static class LogLine
+{
+    public static LogLevel ParseLogLevel(string logLine) => logLine[1..4] switch
+    {
+        "TRC" => LogLevel.Trace,
+        "DBG" => LogLevel.Debug,
+        "INF" => LogLevel.Info,
+        "WRN" => LogLevel.Warning,
+        "ERR" => LogLevel.Error,
+        "FTL" => LogLevel.Fatal,
+        _ => LogLevel.Unknown
+    };
+
+    public static string OutputForShortLog(LogLevel logLevel, string message)
+        => $"{(int)logLevel}:{message}";
+}
