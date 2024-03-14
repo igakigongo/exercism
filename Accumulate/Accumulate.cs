e52e49ba82ef611d3289c05295891a0cd6018d4f
kigongo <igakigongo@gmail.com>
2024-03-14 15:02:39 +0100

    accumulate

---
+++ Accumulate/Accumulate.cs
@@ -0,0 +1,11 @@
+﻿namespace Accumulate;
+
+public static class AccumulateExtensions
+{
+    public static IEnumerable<TU> Accumulate<T, TU>(
+        this IEnumerable<T> collection, Func<T, TU> func)
+    {
+        foreach (var ele in collection)
+            yield return func(ele);
+    }
+}
