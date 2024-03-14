6e6d2c99a83b9fcd0a525fea31f9f30cb1b78c00
kigongo <igakigongo@gmail.com>
2024-03-14 01:05:28 +0100

    nth prime

---
+++ NthPrime/NthPrime.cs
@@ -0,0 +1,41 @@
+﻿namespace NthPrime;
+
+public static class NthPrime
+{
+    public static int Prime(int nth) => nth > 0
+        ? GeneratePrimesToN(nth).ToArray()[^1]
+        : throw new ArgumentOutOfRangeException(nameof(nth));
+
+    private static IEnumerable<int> GeneratePrimesToN(int n)
+    {
+        var count = 0;
+        var curr = 2;
+        while (true)
+        {
+            if (count == n) yield break;
+            if (IsPrime(curr))
+            {
+                count++;
+                yield return curr;
+            }
+
+            curr++;
+        }
+    }
+
+    private static bool IsPrime(int n)
+    {
+        if (n is 2) return true;
+        if ((n & 1) == 0) return false;
+        var limit = (int)Math.Sqrt(n);
+
+        var i = 2;
+        while (i <= limit)
+        {
+            if (n % i == 0) return false;
+            i += (i & 1) == 0 ? 1 : 2; // skip factors of 2
+        }
+
+        return true;
+    }
+}
---
+++ NthPrime/NthPrimeTests.cs
@@ -0,0 +1,36 @@
+using Xunit;
+
+namespace NthPrime;
+
+public class NthPrimeTests
+{
+    [Fact]
+    public void First_prime()
+    {
+        Assert.Equal(2, NthPrime.Prime(1));
+    }
+
+    [Fact]
+    public void Second_prime()
+    {
+        Assert.Equal(3, NthPrime.Prime(2));
+    }
+
+    [Fact]
+    public void Sixth_prime()
+    {
+        Assert.Equal(13, NthPrime.Prime(6));
+    }
+
+    [Fact]
+    public void Big_prime()
+    {
+        Assert.Equal(104743, NthPrime.Prime(10001));
+    }
+
+    [Fact]
+    public void There_is_no_zeroth_prime()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => NthPrime.Prime(0));
+    }
+}
