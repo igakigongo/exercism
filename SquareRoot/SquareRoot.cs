ad50205386b6d801ced7a9cdc48a76661f60f32f
kigongo <igakigongo@gmail.com>
2025-02-24 21:56:53 +0100

    square root

---
+++ SquareRoot/SquareRoot.cs
@@ -0,0 +1,26 @@
+﻿namespace SquareRoot;
+
+public static class SquareRoot
+{
+    public static int Root(int number)
+    {
+        var left = 0;
+        var right = number + 1;
+
+        while (left != right - 1)
+        {
+            var middle = (left + right) / 2;
+
+            if (middle * middle <= number)
+            {
+                left = middle;
+            }
+            else
+            {
+                right = middle;
+            }
+        }
+
+        return left;
+    }
+}
