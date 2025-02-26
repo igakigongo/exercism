e69ac136884392ef49630ac04cc6ae7ca383a9c2
kigongo <igakigongo@gmail.com>
2025-02-26 01:04:33 +0100

    phone number

---
+++ PhoneNumber/PhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PhoneNumber;
+
+public static class PhoneNumber
+{
+    private static readonly Regex Regex = new("^1?([2-9][0-9]{2}){2}[0-9]{4}$");
+    
+    public static string Clean(string phoneNumber)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(phoneNumber);
+        ArgumentException.ThrowIfNullOrWhiteSpace(phoneNumber);
+
+        var phone = new string(phoneNumber
+            .Where(char.IsDigit).ToArray());
+
+        if (phone.Length is > 11 or < 10) 
+            throw new ArgumentException(null, nameof(phoneNumber));
+
+        return Regex.IsMatch(phone)
+            ? phone[^10..]
+            : throw new ArgumentException(null, nameof(phoneNumber));
+    }
+}
---
+++ PhoneNumber/PhoneNumberTests.cs
@@ -0,0 +1,114 @@
+using Xunit;
+
+namespace PhoneNumber;
+
+public class PhoneNumberTests
+{
+    [Fact]
+    public void Cleans_the_number()
+    {
+        Assert.Equal("2234567890", PhoneNumber.Clean("(223) 456-7890"));
+    }
+
+    [Fact]
+    public void Cleans_numbers_with_dots()
+    {
+        Assert.Equal("2234567890", PhoneNumber.Clean("223.456.7890"));
+    }
+
+    [Fact]
+    public void Cleans_numbers_with_multiple_spaces()
+    {
+        Assert.Equal("2234567890", PhoneNumber.Clean("223 456   7890   "));
+    }
+
+    [Fact]
+    public void Invalid_when_9_digits()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("123456789"));
+    }
+
+    [Fact]
+    public void Invalid_when_11_digits_does_not_start_with_a_1()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("22234567890"));
+    }
+
+    [Fact]
+    public void Valid_when_11_digits_and_starting_with_1()
+    {
+        Assert.Equal("2234567890", PhoneNumber.Clean("12234567890"));
+    }
+
+    [Fact]
+    public void Valid_when_11_digits_and_starting_with_1_even_with_punctuation()
+    {
+        Assert.Equal("2234567890", PhoneNumber.Clean("+1 (223) 456-7890"));
+    }
+
+    [Fact]
+    public void Invalid_when_more_than_11_digits()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("321234567890"));
+    }
+
+    [Fact]
+    public void Invalid_with_letters()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("523-abc-7890"));
+    }
+
+    [Fact]
+    public void Invalid_with_punctuations()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("523-@:!-7890"));
+    }
+
+    [Fact]
+    public void Invalid_if_area_code_starts_with_0()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("(023) 456-7890"));
+    }
+
+    [Fact]
+    public void Invalid_if_area_code_starts_with_1()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("(123) 456-7890"));
+    }
+
+    [Fact]
+    public void Invalid_if_exchange_code_starts_with_0()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("(223) 056-7890"));
+    }
+
+    [Fact]
+    public void Invalid_if_exchange_code_starts_with_1()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("(223) 156-7890"));
+    }
+
+    [Fact]
+    public void Invalid_if_area_code_starts_with_0_on_valid_11_digit_number()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("1 (023) 456-7890"));
+    }
+
+    [Fact]
+    public void Invalid_if_area_code_starts_with_1_on_valid_11_digit_number()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("1 (123) 456-7890"));
+    }
+
+    [Fact]
+    public void Invalid_if_exchange_code_starts_with_0_on_valid_11_digit_number()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("1 (223) 056-7890"));
+    }
+
+    [Fact]
+    public void Invalid_if_exchange_code_starts_with_1_on_valid_11_digit_number()
+    {
+        Assert.Throws<ArgumentException>(() => PhoneNumber.Clean("1 (223) 156-7890"));
+    }
+}
