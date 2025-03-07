fc82c5d30ec7b82f0fd9b349c91cf25f69523255
kigongo <igakigongo@gmail.com>
2025-03-07 20:04:53 +0100

    pythagorean triplets

---
+++ PythagoreanTriplet/PythagoreanTriplet.cs
@@ -0,0 +1,17 @@
+﻿namespace PythagoreanTriplet;
+
+public static class PythagoreanTriplet
+{
+    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
+    {
+        int maxA = sum / 3, maxB = sum / 2;
+        
+        for (var a = 1; a < maxA; a++)
+        for (var b = a + 1; b < maxB; b++)
+        {
+            var c = sum - a - b;
+            if (a + b < c) continue;
+            if (a * a + b * b == c * c) yield return (a, b, c);
+        }
+    }
+}
---
+++ PythagoreanTriplet/PythagoreanTripletTests.cs
@@ -0,0 +1,84 @@
+using Xunit;
+
+namespace PythagoreanTriplet;
+
+public class PythagoreanTripletTests
+{
+    [Fact]
+    public void Triplets_whose_sum_is_12()
+    {
+        (int, int, int)[] expected =
+        [
+            (3, 4, 5)
+        ];
+        Assert.Equal(expected, PythagoreanTriplet.TripletsWithSum(12));
+    }
+
+    [Fact]
+    public void Triplets_whose_sum_is_108()
+    {
+        (int, int, int)[] expected =
+        [
+            (27, 36, 45)
+        ];
+        Assert.Equal(expected, PythagoreanTriplet.TripletsWithSum(108));
+    }
+
+    [Fact]
+    public void Triplets_whose_sum_is_1000()
+    {
+        (int, int, int)[] expected =
+        [
+            (200, 375, 425)
+        ];
+        Assert.Equal(expected, PythagoreanTriplet.TripletsWithSum(1000));
+    }
+
+    [Fact]
+    public void No_matching_triplets_for_1001()
+    {
+        Assert.Empty(PythagoreanTriplet.TripletsWithSum(1001));
+    }
+
+    [Fact]
+    public void Returns_all_matching_triplets()
+    {
+        (int, int, int)[] expected =
+        [
+            (9, 40, 41),
+            (15, 36, 39)
+        ];
+        Assert.Equal(expected, PythagoreanTriplet.TripletsWithSum(90));
+    }
+
+    [Fact]
+    public void Several_matching_triplets()
+    {
+        (int, int, int)[] expected =
+        [
+            (40, 399, 401),
+            (56, 390, 394),
+            (105, 360, 375),
+            (120, 350, 370),
+            (140, 336, 364),
+            (168, 315, 357),
+            (210, 280, 350),
+            (240, 252, 348)
+        ];
+        Assert.Equal(expected, PythagoreanTriplet.TripletsWithSum(840));
+    }
+
+    [Fact]
+    public void Triplets_for_large_number()
+    {
+        (int, int, int)[] expected =
+        [
+            (1200, 14375, 14425),
+            (1875, 14000, 14125),
+            (5000, 12000, 13000),
+            (6000, 11250, 12750),
+            (7500, 10000, 12500)
+        ];
+        Assert.Equal(expected, PythagoreanTriplet.TripletsWithSum(30000));
+    }
+}
