689bccb83913d36b5c3ecc6d453b9eb2f68b085b
kigongo <igakigongo@gmail.com>
2025-03-09 21:28:51 +0100

    bank account

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,60 @@
+﻿namespace BankAccount;
+
+public class BankAccount
+{
+    private readonly object _lock = new();
+    private decimal _balance;
+
+    private bool IsClosed { get; set; } = true;
+
+    public decimal Balance
+    {
+        get
+        {
+            if (IsClosed) throw new InvalidOperationException();
+            lock (_lock)
+            {
+                return _balance;
+            }
+        }
+        private set
+        {
+            lock (_lock)
+            {
+                _balance = value;
+            }
+        }
+    }
+
+    public void Open()
+    {
+        if (IsClosed == false)
+            throw new InvalidOperationException();
+
+        Balance = 0;
+        IsClosed = false;
+    }
+
+    public void Close()
+    {
+        if (IsClosed)
+            throw new InvalidOperationException();
+        IsClosed = true;
+    }
+
+    public void Deposit(decimal change)
+    {
+        if (IsClosed) throw new InvalidOperationException();
+        if (change <= 0) throw new InvalidOperationException();
+        Balance += change;
+    }
+
+    public void Withdraw(decimal change)
+    {
+        if (IsClosed) throw new InvalidOperationException();
+        if (change <= 0) throw new InvalidOperationException();
+        if (change > Balance) throw new InvalidOperationException();
+
+        Balance -= change;
+    }
+}
---
+++ BankAccount/BankAccountTests.cs
@@ -0,0 +1,177 @@
+using Xunit;
+
+namespace BankAccount;
+
+public class BankAccountTests
+{
+    [Fact]
+    public void Newly_opened_account_has_zero_balance()
+    {
+        var account = new BankAccount();
+        account.Open();
+        Assert.Equal(0m, account.Balance);
+    }
+
+    [Fact]
+    public void Single_deposit()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Deposit(100m);
+        Assert.Equal(100m, account.Balance);
+    }
+
+    [Fact]
+    public void Multiple_deposits()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Deposit(100m);
+        account.Deposit(50m);
+        Assert.Equal(150m, account.Balance);
+    }
+
+    [Fact]
+    public void Withdraw_once()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Deposit(100m);
+        account.Withdraw(75m);
+        Assert.Equal(25m, account.Balance);
+    }
+
+    [Fact]
+    public void Withdraw_twice()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Deposit(100m);
+        account.Withdraw(80m);
+        account.Withdraw(20m);
+        Assert.Equal(0m, account.Balance);
+    }
+
+    [Fact]
+    public void Can_do_multiple_operations_sequentially()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Deposit(100m);
+        account.Deposit(110m);
+        account.Withdraw(200m);
+        account.Deposit(60m);
+        account.Withdraw(50m);
+        Assert.Equal(20m, account.Balance);
+    }
+
+    [Fact]
+    public void Cannot_check_balance_of_closed_account()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Close();
+        Assert.Throws<InvalidOperationException>(() => account.Balance);
+    }
+
+    [Fact]
+    public void Cannot_deposit_into_closed_account()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Close();
+        Assert.Throws<InvalidOperationException>(() => account.Deposit(50m));
+    }
+
+    [Fact]
+    public void Cannot_deposit_into_unopened_account()
+    {
+        var account = new BankAccount();
+        Assert.Throws<InvalidOperationException>(() => account.Deposit(50m));
+    }
+
+    [Fact]
+    public void Cannot_withdraw_from_closed_account()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Close();
+        Assert.Throws<InvalidOperationException>(() => account.Withdraw(50m));
+    }
+
+    [Fact]
+    public void Cannot_close_an_account_that_was_not_opened()
+    {
+        var account = new BankAccount();
+        Assert.Throws<InvalidOperationException>(() => account.Close());
+    }
+
+    [Fact]
+    public void Cannot_open_an_already_opened_account()
+    {
+        var account = new BankAccount();
+        account.Open();
+        Assert.Throws<InvalidOperationException>(() => account.Open());
+    }
+
+    [Fact]
+    public void Reopened_account_does_not_retain_balance()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Deposit(50m);
+        account.Close();
+        account.Open();
+        Assert.Equal(0m, account.Balance);
+    }
+
+    [Fact]
+    public void Cannot_withdraw_more_than_deposited()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Deposit(25m);
+        Assert.Throws<InvalidOperationException>(() => account.Withdraw(50m));
+    }
+
+    [Fact]
+    public void Cannot_withdraw_negative()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Deposit(100m);
+        Assert.Throws<InvalidOperationException>(() => account.Withdraw(-50m));
+    }
+
+    [Fact]
+    public void Cannot_deposit_negative()
+    {
+        var account = new BankAccount();
+        account.Open();
+        Assert.Throws<InvalidOperationException>(() => account.Deposit(-50m));
+    }
+
+    [Fact]
+    public async Task Can_handle_concurrent_transactions()
+    {
+        var account = new BankAccount();
+        account.Open();
+        for (var i = 0; i < 500; i++)
+        {
+            var tasks = new List<Task>
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    for (var j = 0; j < 100; j++)
+                    {
+                        account.Deposit(1m);
+                        account.Withdraw(1m);
+                    }
+                })
+            };
+            await Task.WhenAll(tasks.ToArray());
+        }
+
+        Assert.Equal(0m, account.Balance);
+    }
+}
