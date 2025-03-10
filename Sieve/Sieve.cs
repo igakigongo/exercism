4d4295afcd51fe967c833cc62114f9603f3cccd3
kigongo <igakigongo@gmail.com>
2025-03-10 23:49:29 +0100

    sieve

---
+++ Sieve/Sieve.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace Sieve;
+
+public static class Sieve
+{
+    public static IEnumerable<int> Primes(int limit)
+    {
+        var array = new BitArray(limit + 1, true);
+
+        for (var i = 2; i <= limit; i++)
+        {
+            if (array[i] == false) continue;
+            for (var j = i * 2; j <= limit; j += i)
+                array[j] = false;
+
+            yield return i;
+        }
+    }
+}
