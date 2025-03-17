ac3eba720db5fd3c903e944a850c1c01148599fc
kigongo <igakigongo@gmail.com>
2025-03-17 22:13:32 +0100

    sublist

---
+++ Sublist/Sublist.cs
@@ -0,0 +1,37 @@
+﻿namespace Sublist;
+
+public enum SublistType
+{
+    Equal,
+    Unequal,
+    Superlist,
+    Sublist
+}
+
+public static class Sublist
+{
+    public static SublistType Classify<T>(List<T> first, List<T> second)
+        where T : IComparable
+    {
+        if (first.Count == second.Count && first.SequenceEqual(second))
+            return SublistType.Equal;
+
+        if (first.Count < second.Count && second.ContainsAll(first))
+            return SublistType.Sublist;
+
+        if (first.Count > second.Count && first.ContainsAll(second))
+            return SublistType.Superlist;
+
+        return SublistType.Unequal;
+    }
+
+    private static bool ContainsAll<T>(this List<T> first, List<T> second)
+    {
+        var end = first.Count - second.Count;
+        for (var i = 0; i <= end; i++)
+            if (first.GetRange(i, second.Count).SequenceEqual(second))
+                return true;
+
+        return false;
+    }
+}
---
+++ Sublist/SublistTests.cs
@@ -0,0 +1,150 @@
+using Xunit;
+
+namespace Sublist;
+
+public class SublistTests
+{
+    [Fact]
+    public void Empty_lists()
+    {
+        List<int> listOne = [];
+        List<int> listTwo = [];
+        Assert.Equal(SublistType.Equal, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Empty_list_within_non_empty_list()
+    {
+        List<int> listOne = [];
+        List<int> listTwo = [1, 2, 3];
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Non_empty_list_contains_empty_list()
+    {
+        List<int> listOne = [1, 2, 3];
+        List<int> listTwo = [];
+        Assert.Equal(SublistType.Superlist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void List_equals_itself()
+    {
+        List<int> listOne = [1, 2, 3];
+        List<int> listTwo = [1, 2, 3];
+        Assert.Equal(SublistType.Equal, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Different_lists()
+    {
+        List<int> listOne = [1, 2, 3];
+        List<int> listTwo = [2, 3, 4];
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void False_start()
+    {
+        List<int> listOne = [1, 2, 5];
+        List<int> listTwo = [0, 1, 2, 3, 1, 2, 5, 6];
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Consecutive()
+    {
+        List<int> listOne = [1, 1, 2];
+        List<int> listTwo = [0, 1, 1, 1, 2, 1, 2];
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Sublist_at_start()
+    {
+        List<int> listOne = [0, 1, 2];
+        List<int> listTwo = [0, 1, 2, 3, 4, 5];
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Sublist_in_middle()
+    {
+        List<int> listOne = [2, 3, 4];
+        List<int> listTwo = [0, 1, 2, 3, 4, 5];
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Sublist_at_end()
+    {
+        List<int> listOne = [3, 4, 5];
+        List<int> listTwo = [0, 1, 2, 3, 4, 5];
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void At_start_of_superlist()
+    {
+        List<int> listOne = [0, 1, 2, 3, 4, 5];
+        List<int> listTwo = [0, 1, 2];
+        Assert.Equal(SublistType.Superlist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void In_middle_of_superlist()
+    {
+        List<int> listOne = [0, 1, 2, 3, 4, 5];
+        List<int> listTwo = [2, 3];
+        Assert.Equal(SublistType.Superlist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void At_end_of_superlist()
+    {
+        List<int> listOne = [0, 1, 2, 3, 4, 5];
+        List<int> listTwo = [3, 4, 5];
+        Assert.Equal(SublistType.Superlist, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void First_list_missing_element_from_second_list()
+    {
+        List<int> listOne = [1, 3];
+        List<int> listTwo = [1, 2, 3];
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Second_list_missing_element_from_first_list()
+    {
+        List<int> listOne = [1, 2, 3];
+        List<int> listTwo = [1, 3];
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void First_list_missing_additional_digits_from_second_list()
+    {
+        List<int> listOne = [1, 2];
+        List<int> listTwo = [1, 22];
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Order_matters_to_a_list()
+    {
+        List<int> listOne = [1, 2, 3];
+        List<int> listTwo = [3, 2, 1];
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(listOne, listTwo));
+    }
+
+    [Fact]
+    public void Same_digits_but_different_numbers()
+    {
+        List<int> listOne = [1, 0, 1];
+        List<int> listTwo = [10, 1];
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(listOne, listTwo));
+    }
+}
