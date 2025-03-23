26db6bd814419309d57ea14b724b2615b3e3dad6
kigongo <igakigongo@gmail.com>
2025-03-23 15:39:08 +0100

    word count

---
+++ WordCount/WordCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordCount;
+
+public static class WordCount
+{
+    private static readonly Regex Regex = new(@"\b\w+[']?\w*\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+    public static IDictionary<string, int> CountWords(string phrase)
+    {
+        Dictionary<string, int> dict = [];
+        var matches = Regex.Matches(phrase);
+        foreach (Match match in matches)
+        {
+            var str = match.Value.ToLowerInvariant();
+            dict[str] = dict.TryGetValue(str, out var count) ? count + 1 : 1;
+        }
+
+        return dict;
+    }
+}
