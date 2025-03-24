2bfc0202dd85ce564d0d3cec375186731da43a94
kigongo <igakigongo@gmail.com>
2025-03-24 21:47:52 +0100

    deque

---
+++ LinkedList/Deque.cs
@@ -0,0 +1,61 @@
+﻿namespace LinkedList;
+
+public class Node<T>
+{
+    public required T Data { get; set; }
+    public Node<T>? Previous { get; set; }
+    public Node<T>? Next { get; set; }
+}
+
+public class Deque<T>
+{
+    private Node<T>? _head;
+
+    public void Push(T value)
+    {
+        if (_head == null)
+            _head = new Node<T> { Data = value };
+        else
+        {
+            var current = _head;
+            while (current.Next != null)
+                current = current.Next;
+
+            current.Next = new Node<T> { Data = value, Previous = current, Next = null };
+        }
+    }
+
+    public T Pop()
+    {
+        var current = _head;
+        if (current == null) 
+            throw new InvalidOperationException("Deque is empty");
+        
+        while (current.Next != null)
+            current = current.Next;
+
+        if (current.Previous != null)
+            current.Previous.Next = null;
+        else
+            _head = null; // if there was only one node
+        
+        return current.Data;
+    }
+
+    public void Unshift(T value)
+    {
+        var node = new Node<T> { Data = value, Previous = null, Next = _head };
+        if (_head != null)
+            _head.Previous = node;
+
+        _head = node;
+    }
+
+    public T Shift()
+    {
+        if (_head == null) throw new InvalidOperationException("Deque is empty");
+        var data = _head.Data;
+        _head = _head.Next;
+        return data;
+    }
+}
---
+++ LinkedList/DequeTests.cs
@@ -0,0 +1,92 @@
+using Xunit;
+
+namespace LinkedList;
+
+public class DequeTests
+{
+    [Fact]
+    public void Pop_gets_element_from_the_list()
+    {
+        var sut = new Deque<int>();
+        sut.Push(7);
+        Assert.Equal(7, sut.Pop());
+    }
+
+    [Fact]
+    public void Push_pop_respectively_add_remove_at_the_end_of_the_list()
+    {
+        var sut = new Deque<int>();
+        sut.Push(11);
+        sut.Push(13);
+        Assert.Equal(13, sut.Pop());
+        Assert.Equal(11, sut.Pop());
+    }
+
+    [Fact]
+    public void Shift_gets_an_element_from_the_list()
+    {
+        var sut = new Deque<int>();
+        sut.Push(17);
+        Assert.Equal(17, sut.Shift());
+    }
+
+    [Fact]
+    public void Shift_gets_first_element_from_the_list()
+    {
+        var sut = new Deque<int>();
+        sut.Push(23);
+        sut.Push(5);
+        Assert.Equal(23, sut.Shift());
+        Assert.Equal(5, sut.Shift());
+    }
+
+    [Fact]
+    public void Unshift_adds_element_at_start_of_the_list()
+    {
+        var sut = new Deque<int>();
+        sut.Unshift(23);
+        sut.Unshift(5);
+        Assert.Equal(5, sut.Shift());
+        Assert.Equal(23, sut.Shift());
+    }
+
+    [Fact]
+    public void Pop_push_shift_and_unshift_can_be_used_in_any_order()
+    {
+        var sut = new Deque<int>();
+        sut.Push(1);
+        sut.Push(2);
+        Assert.Equal(2, sut.Pop());
+        sut.Push(3);
+        Assert.Equal(1, sut.Shift());
+        sut.Unshift(4);
+        sut.Push(5);
+        Assert.Equal(4, sut.Shift());
+        Assert.Equal(5, sut.Pop());
+        Assert.Equal(3, sut.Shift());
+    }
+
+    [Fact]
+    public void Popping_to_empty_doesn_t_break_the_list()
+    {
+        var sut = new Deque<int>();
+        sut.Push(41);
+        sut.Push(59);
+        sut.Pop();
+        sut.Pop();
+        sut.Push(47);
+        Assert.Equal(47, sut.Pop());
+    }
+
+    [Fact]
+    public void Shifting_to_empty_doesn_t_break_the_list()
+    {
+        var sut = new Deque<int>();
+        sut.Push(41);
+        sut.Push(59);
+        sut.Shift();
+        sut.Shift();
+        sut.Push(47);
+        Assert.Equal(47, sut.Shift());
+    }
+}
