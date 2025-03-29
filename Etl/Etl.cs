8d1efd98b8791158c46f03b1596a531d7def2c55
kigongo <igakigongo@gmail.com>
2025-03-29 19:17:10 +0100

    etl

---
+++ Etl/Etl.cs
@@ -0,0 +1,14 @@
+﻿namespace Etl;
+
+public static class Etl
+{
+    public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
+    {
+        Dictionary<string, int> dictionary = [];
+        foreach (var (key, value) in old)
+        foreach (var letter in value)
+            dictionary[letter.ToLowerInvariant()] = key;
+
+        return dictionary;
+    }
+}
---
+++ Etl/EtlTests.cs
@@ -0,0 +1,101 @@
+using Xunit;
+
+namespace Etl;
+
+public class EtlTests
+{
+    [Fact]
+    public void Single_letter()
+    {
+        var legacy = new Dictionary<int, string[]>
+        {
+            [1] = ["A"]
+        };
+        var expected = new Dictionary<string, int>
+        {
+            ["a"] = 1
+        };
+        Assert.Equal(expected, Etl.Transform(legacy));
+    }
+
+    [Fact]
+    public void Single_score_with_multiple_letters()
+    {
+        var legacy = new Dictionary<int, string[]>
+        {
+            [1] = ["A", "E", "I", "O", "U"]
+        };
+        var expected = new Dictionary<string, int>
+        {
+            ["a"] = 1,
+            ["e"] = 1,
+            ["i"] = 1,
+            ["o"] = 1,
+            ["u"] = 1
+        };
+        Assert.Equal(expected, Etl.Transform(legacy));
+    }
+
+    [Fact]
+    public void Multiple_scores_with_multiple_letters()
+    {
+        var legacy = new Dictionary<int, string[]>
+        {
+            [1] = ["A", "E"],
+            [2] = ["D", "G"]
+        };
+        var expected = new Dictionary<string, int>
+        {
+            ["a"] = 1,
+            ["d"] = 2,
+            ["e"] = 1,
+            ["g"] = 2
+        };
+        Assert.Equal(expected, Etl.Transform(legacy));
+    }
+
+    [Fact]
+    public void Multiple_scores_with_differing_numbers_of_letters()
+    {
+        var legacy = new Dictionary<int, string[]>
+        {
+            [1] = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+            [2] = ["D", "G"],
+            [3] = ["B", "C", "M", "P"],
+            [4] = ["F", "H", "V", "W", "Y"],
+            [5] = ["K"],
+            [8] = ["J", "X"],
+            [10] = ["Q", "Z"]
+        };
+        var expected = new Dictionary<string, int>
+        {
+            ["a"] = 1,
+            ["b"] = 3,
+            ["c"] = 3,
+            ["d"] = 2,
+            ["e"] = 1,
+            ["f"] = 4,
+            ["g"] = 2,
+            ["h"] = 4,
+            ["i"] = 1,
+            ["j"] = 8,
+            ["k"] = 5,
+            ["l"] = 1,
+            ["m"] = 3,
+            ["n"] = 1,
+            ["o"] = 1,
+            ["p"] = 3,
+            ["q"] = 10,
+            ["r"] = 1,
+            ["s"] = 1,
+            ["t"] = 1,
+            ["u"] = 1,
+            ["v"] = 4,
+            ["w"] = 4,
+            ["x"] = 8,
+            ["y"] = 4,
+            ["z"] = 10
+        };
+        Assert.Equal(expected, Etl.Transform(legacy));
+    }
+}
