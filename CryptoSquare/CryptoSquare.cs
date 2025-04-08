188b56ba8f0b0488b39a08d4094e29463b087750
kigongo <igakigongo@gmail.com>
2025-04-09 00:27:08 +0200

    crypto square

---
+++ CryptoSquare/CryptoSquare.cs
@@ -0,0 +1,46 @@
+﻿namespace CryptoSquare;
+
+public static class CryptoSquare
+{
+    private static string NormalizedPlaintext(string plaintext) => new(plaintext
+        .Where(char.IsLetterOrDigit)
+        .Select(char.ToLower).ToArray());
+
+    public static string Ciphertext(string plaintext)
+    {
+        var normalizedPlaintext = NormalizedPlaintext(plaintext);
+        var (rows, cols) = GetSquareDimensions(normalizedPlaintext.Length);
+
+        var len = normalizedPlaintext.Length;
+        var words = new List<string>();
+
+        for (var row = 0; row < rows; row++)
+        {
+            var str = new char[cols];
+            for (var col = 0; col < cols; col++)
+            {
+                var idx = col * rows + row;
+                var ch = idx < len ? normalizedPlaintext[idx] : ' ';
+                str[col] = ch;
+            }
+
+            words.Add(new string(str));
+        }
+
+        return string.Join(" ", words);
+    }
+
+    private static (int r, int c) GetSquareDimensions(int limitOfArea)
+    {
+        int r = (int)Math.Floor(Math.Sqrt(limitOfArea)), c = r;
+        while (true)
+        {
+            if (r * c >= limitOfArea) break;
+            c += 1;
+            if (r * c >= limitOfArea) break;
+            r++;
+        }
+
+        return r > c ? (r, c) : (c, r);
+    }
+}
---
+++ CryptoSquare/CryptoSquareTests.cs
@@ -0,0 +1,55 @@
+using Xunit;
+
+namespace CryptoSquare;
+
+public class CryptoSquareTests
+{
+    [Fact]
+    public void Empty_plaintext_results_in_an_empty_ciphertext()
+    {
+        Assert.Equal("", CryptoSquare.Ciphertext(""));
+    }
+
+    [Fact]
+    public void Normalization_results_in_empty_plaintext()
+    {
+        Assert.Equal("", CryptoSquare.Ciphertext("... --- ..."));
+    }
+
+    [Fact]
+    public void Lowercase()
+    {
+        Assert.Equal("a", CryptoSquare.Ciphertext("A"));
+    }
+
+    [Fact]
+    public void Remove_spaces()
+    {
+        Assert.Equal("b", CryptoSquare.Ciphertext("  b "));
+    }
+
+    [Fact]
+    public void Remove_punctuation()
+    {
+        Assert.Equal("1", CryptoSquare.Ciphertext("@1,%!"));
+    }
+
+    [Fact]
+    public void Nine_character_plaintext_results_in_3_chunks_of_3_characters()
+    {
+        Assert.Equal("tsf hiu isn", CryptoSquare.Ciphertext("This is fun!"));
+    }
+
+    [Fact]
+    public void Eight_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_space()
+    {
+        Assert.Equal("clu hlt io ", CryptoSquare.Ciphertext("Chill out."));
+    }
+
+    [Fact]
+    public void Fifty_four_character_plaintext_results_in_7_chunks_the_last_two_with_trailing_spaces()
+    {
+        Assert.Equal("imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau ",
+            CryptoSquare.Ciphertext("If man was meant to stay on the ground, god would have given us roots."));
+    }
+}
